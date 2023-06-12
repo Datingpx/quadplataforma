8f60f6fad0b0d8ecd82cd3b693cafc742950b67c
Datingpx <gafegf12@gmail.com>
2023-06-12 15:04:23 -0300

    primeiro commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject hazardPrefab;
+
+    public int maxHazardsToSpawn = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnHazards());
+    }
+    private IEnumerator SpawnHazards()
+    {
+        var hazardsToSpawn = Random.Range(1, maxHazardsToSpawn);
+
+        for (int i = 0; i < hazardsToSpawn; i++)
+        {
+            var x = Random.Range(-7, 7);
+            var drag = Random.Range(0f, 2f);
+            var hazard = Instantiate(hazardPrefab, new Vector3(x, 11, 0), Quaternion.identity);
+            hazard.GetComponent<Rigidbody>().drag = drag;
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        yield return SpawnHazards();
+    }
+
+}
